e38237e803d80dea59b042640cabfd203ee09a5e
Administrator <admin@example.com>
2022-01-15 18:01:29 +0700

    update content web

--- web.go
+++ web.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"net/http"
 )
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "The Trong TDT asia \n Thai Binh \n")
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "The Trong 1999 \n Thai Binh \n")
 	})
 	http.ListenAndServe(":8463", nil)
 }
