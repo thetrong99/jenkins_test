e8ba732a240af2d96eb1ab5ec0279940479e0722
Administrator <admin@example.com>
2022-01-15 18:14:36 +0700

    ok

--- web.go
+++ web.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "The Trong 2710 1999 \n Thai Binh \n")
+		fmt.Fprintf(w, "The Trong 2710 1999 no1 \n Thai Binh \n")
 	})
 	http.ListenAndServe(":8463", nil)
 }
